3c149b514145be0e8b0a8c0eee7cfb29bbd2eedb
swing <qianaokai@gmail.com>
2020-06-12 20:44:51 +0800

    update
    该banner

---
+++ back_end/diancan/src/main/java/com/order/diancan/controller/AdminController.java
@@ -0,0 +1,9 @@
+package com.order.diancan.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/admin")
+public class AdminController {
+}
---
+++ back_end/diancan/src/main/java/com/order/diancan/mapper/AdminMapper.java
@@ -0,0 +1,21 @@
+package com.order.diancan.mapper;
+
+import com.order.diancan.bean.Admin;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+@Mapper
+public interface AdminMapper {
+    //insert注册用
+    @Insert("INSERT INTO admin(account,password,restaurant_id) VALUE (#{account},#{password},#{restaurant_id})")
+    int insert(@Param("account") String account,
+               @Param("password") String password,
+               @Param("restaurant_id") long restaurant_id
+    );
+
+    //select登录用
+    @Select("SELECT * FROM customer WHERE account=#{account}")
+    Admin select(@Param("account") String account);
+}
