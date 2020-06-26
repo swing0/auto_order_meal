63919dea841bdab3835a30f1fda4e3fa9791050c
YataoFu <66727485+YataoFu@users.noreply.github.com>
2020-06-26 10:12:07 +0800

    1

--- front_end/front/src/components/Login.vue
+++ front_end/front/src/components/Login.vue
@@ -17,7 +17,7 @@
         <!-- 按钮区域 -->
         <el-form-item class="btns">
           <el-button type="primary" @click="login">登录</el-button>
-          <el-button type="info" >重置</el-button>
+          <!-- <el-button type="info" >重置</el-button> -->
         </el-form-item>
       </el-form>
     </div>
